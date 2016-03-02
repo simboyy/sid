171be7851ef00663bd040e9696e03a43b18f18ed
simboyy <smkorera@gmail.com>
2016-03-02 16:08:59 +0200

    make it better

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@
 'use strict';
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var express = require('express');
 var mongoose = require('mongoose');
 var config = require('./config/environment');
 
 // Connect to database
-mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://simba:1994kingsss@ds019698.mlab.com:19698/heroku_45t33txd');
 mongoose.connection.on('error', function(err) {
 	console.error('MongoDB connection error: ' + err);
 	process.exit(-1);
