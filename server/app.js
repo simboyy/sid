d373a7f0d11f32cd155d11828556146c57cfe088
simboyy <smkorera@gmail.com>
2016-03-07 23:37:21 +0200

    update 4

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ var config = require('./config/environment');
 
 // Connect to database
 mongoose.connect(process.env.MONGOLAB_URI);
+// mongoose.connect(config.mongo.uri, config.mongo.options);
 mongoose.connection.on('error', function(err) {
 	console.error('MongoDB connection error: ' + err);
 	process.exit(-1);
